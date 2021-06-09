6a0741676fd051fabe3778fc8f60987f5268651c
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2021-06-09 19:24:56 -0300

    renombro index

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.use(require('./routes/index'));
+app.use(require('./routes/persona'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/libro'));
+
+var config = require('./config');
+const port = config.server.port;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
+
+exports.app = app;

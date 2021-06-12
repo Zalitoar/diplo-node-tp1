a0ac1a0660242551870c1d66083c0e1eed9bff7d
Michelle <michidamario@gmail.com>
2021-06-12 19:46:34 -0300

    querys de persona sin validaciones

--- routes/persona.js
+++ routes/persona.js
@@ -16,8 +16,11 @@ router.get('/persona', (req, res) => {
 });
 
 router.get('/persona/:id', (req, res) => {
-    res.json({ "una": "persona" });
-});
+    db.query('SELECT * FROM persona WHERE id=?',[req.params.id], (err, rows) => { 
+        if (err) throw err;
+        res.json(rows);
+    });
+}); //validaciones del get
 
 router.post("/persona", (req, res) => {
     try {
@@ -36,13 +39,37 @@ router.post("/persona", (req, res) => {
         res.json(e.message);
     }
 });
+//hacer validacion de "faltan datos" en persona
 
 router.put('/persona/:id', (req, res) => {
-    res.json({ "una": "persona" });
-});
+    try{
+        db.query("INSERT INTO persona (nombre, apellido, alias, email) values (?,?,?,?)", [req.body.nombre, req.body.apellido, req.body.alias, req.body.email], (error, registro, campos) => {
+            if (error) {
+                throw new Error("error al ingresar en la base de datos");
+            }
+            return registro;
+        });
+    }
+    catch (e) {
+        res.status(413).json(e.message);  
+    }
+}); //to do: hacer las validaciones de edicion 
 
 router.delete('/persona/:id', (req, res) => {
-    res.json({ "una": "persona" });
+    try{
+        db.query("DELETE * FROM persona WHERE id=?", [req.params.id], (error, registro, campos)=>{
+         if (error){
+            throw new Error(error.message);
+            }
+            return registro;
+        });
+
+        res.status(200).json({ "message": "se borro correctamente" }); 
+    }
+    catch (e) {
+        //to do: hacer validaciones para delete
+
+    }
 });
 
 module.exports = router;

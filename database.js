a32cefaeec1bb60f0a74cb100f20e2158214d258
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2021-06-14 22:57:46 -0300

    formateo database

--- database.js
+++ database.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-var config = require('./config');
-const mysql = require('mysql');
+var config = require("./config");
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: config.db.host,
-    user: config.db.user,
-    password: config.db.pwd,
-    database: config.db.name,
-    insecureAuth: true
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.pwd,
+  database: config.db.name,
+  insecureAuth: true,
 });
 
 db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
+  if (err) throw err;
+  console.log("Connected!");
 });
 
 module.exports = db;
